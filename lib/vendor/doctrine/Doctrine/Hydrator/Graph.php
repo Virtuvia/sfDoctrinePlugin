a905048d93f5d86a0fa70809777a103f8d15f6f0
Matt Lehner <matt.lehner@coachmeplus.com>
2019-05-24 15:06:30 -0400

    replace each with key/current

--- lib/vendor/doctrine/Doctrine/Hydrator/Graph.php
+++ lib/vendor/doctrine/Doctrine/Hydrator/Graph.php
@@ -416,7 +416,8 @@ abstract class Doctrine_Hydrator_Graph extends Doctrine_Hydrator_Abstract
                     $matchedComponents[] = $table->getComponentName();
                 }
             } else {
-                list($key, $value) = current($inheritanceMap);
+                $key = key($inheritanceMap);
+                $value = $key === null ? null : current($inheritanceMap);
                 $key = $this->_tables[$component]->getFieldName($key);
                 if ( ! isset($data[$key]) || $data[$key] != $value) {
                     continue;
