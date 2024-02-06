88faa0eac04a78005b48d9b38e2e36878162559a
synace <synace@gmail.com>
2024-02-06 10:15:56 -0500

    match singular parameter name

--- lib/validator/sfValidatorDoctrineUnique.class.php
+++ lib/validator/sfValidatorDoctrineUnique.class.php
@@ -66,40 +66,40 @@ class sfValidatorDoctrineUnique extends sfValidatorSchema
   /**
    * @see sfValidatorBase
    */
-  protected function doClean($values)
+  protected function doClean($value)
   {
-    $originalValues = $values;
+    $originalValues = $value;
     $table = Doctrine_Core::getTable($this->getOption('model'));
     if (!is_array($this->getOption('column')))
     {
       $this->setOption('column', array($this->getOption('column')));
     }
 
-    //if $values isn't an array, make it one
-    if (!is_array($values))
+    //if $value isn't an array, make it one
+    if (!is_array($value))
     {
       //use first column for key
       $columns = $this->getOption('column');
-      $values = array($columns[0] => $values);
+      $value = array($columns[0] => $value);
     }
 
     $q = Doctrine_Core::getTable($this->getOption('model'))->createQuery('a');
     foreach ($this->getOption('column') as $column)
     {
       $colName = $table->getColumnName($column);
-      if (!array_key_exists($column, $values))
+      if (!array_key_exists($column, $value))
       {
         // one of the column has be removed from the form
         return $originalValues;
       }
 
-      $q->addWhere('a.' . $colName . ' = ?', $values[$column]);
+      $q->addWhere('a.' . $colName . ' = ?', $value[$column]);
     }
 
     $object = $q->fetchOne();
 
     // if no object or if we're updating the object, it's ok
-    if (!$object || $this->isUpdate($object, $values))
+    if (!$object || $this->isUpdate($object, $value))
     {
       return $originalValues;
     }
