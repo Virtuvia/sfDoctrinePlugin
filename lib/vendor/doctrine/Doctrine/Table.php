1cb1c7cf6ab8bf271271d255abe6807cf9f3e6d0
synace <synace@gmail.com>
2024-02-13 12:27:09 -0500

    Doctrine_Query::buildSqlQuery calls processOrderBy with a possibly null orderby

--- lib/vendor/doctrine/Doctrine/Table.php
+++ lib/vendor/doctrine/Doctrine/Table.php
@@ -1158,7 +1158,7 @@ class Doctrine_Table extends Doctrine_Configurable implements Countable
         }
 
         if ( ! is_array($orderBy)) {
-            $e1 = explode(',', $orderBy);
+            $e1 = explode(',', $orderBy ?? '');
         } else {
             $e1 = $orderBy;
         }
