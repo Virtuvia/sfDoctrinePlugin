00889c424affd20badda9de270d64ffa7ccfc76b
Matt Lehner <matt.lehner@coachmeplus.com>
2024-04-23 00:23:44 -0400

    bump version to 1.2.4.21

--- lib/vendor/doctrine/Doctrine/Core.php
+++ lib/vendor/doctrine/Doctrine/Core.php
@@ -35,7 +35,7 @@ class Doctrine_Core
     /**
      * VERSION
      */
-    const VERSION                   = '1.2.4.20';
+    const VERSION                   = '1.2.4.21';
 
     /**
      * ERROR CONSTANTS
