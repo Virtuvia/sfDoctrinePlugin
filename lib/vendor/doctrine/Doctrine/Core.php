9d7ebe295eed26c35da7a242fdeecfa3b975a72b
Stephen Ostrow <stephen@coachmeplus.com>
2024-05-29 20:49:21 -0400

    bump version to 1.2.4.22

--- lib/vendor/doctrine/Doctrine/Core.php
+++ lib/vendor/doctrine/Doctrine/Core.php
@@ -35,7 +35,7 @@ class Doctrine_Core
     /**
      * VERSION
      */
-    const VERSION                   = '1.2.4.21';
+    const VERSION                   = '1.2.4.22';
 
     /**
      * ERROR CONSTANTS
