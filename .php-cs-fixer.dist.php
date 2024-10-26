2aecba17d21802a9beaae586ed0aedc0e1b97730
Matt Lehner <matt.lehner@coachmeplus.com>
2024-10-26 14:06:02 -0400

    update php-cs-fixer to @PER-CS2.0

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,23 +13,9 @@ $finder = (new PhpCsFixer\Finder())
 $config = new \PhpCsFixer\Config();
 $config
     ->setRules([
-        '@PSR12' => true,
+        '@PER-CS2.0' => true,
         'method_argument_space' => ['on_multiline' => 'ignore'],
-
-        'encoding' => true,
-        'no_trailing_whitespace_in_comment' => true,
-        'no_trailing_whitespace' => true,
-        'line_ending' => true,
-        'no_whitespace_in_blank_line' => true,
-        'single_blank_line_at_eof' => true,
-
-        'array_indentation' => true,
-        'statement_indentation' => true,
-        'heredoc_indentation' => false,
-        'method_chaining_indentation' => false,
-        'indentation_type' => true,
-
-        'no_extra_blank_lines' => true,
+        'single_line_empty_body' => false,
     ])
     ->setFinder($finder)
 ;
